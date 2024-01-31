0124f40e30e9f760514d293f1f42607a49ea53eb
Sneh Kumar Patidar <snehkumarpatidar75820@gmail.com>
2024-01-31 11:07:12 +0530

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+
+const About = () => {
+
+  
+  return (
+   
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-8 offset-md-2">
+          <div className="card">
+            <div className="card-body">
+              <h1 className="card-title text-center mb-4">About iNotebook</h1>
+              <p className="card-text">
+                Welcome to iNotebook, your digital notebook solution! iNotebook is a simple yet powerful note-taking application designed to help you organize your thoughts, ideas, tasks, and more. With iNotebook, you can easily create, edit, and manage your notes from anywhere, at any time.
+              </p>
+              <p className="card-text">
+                Whether you're a student, professional, or just someone who likes to jot down thoughts on the go, iNotebook is the perfect tool for you. Our intuitive interface and user-friendly features make it easy to stay organized and productive.
+              </p>
+              <p className="card-text">
+                Get started with iNotebook today and take control of your notes like never before!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About
